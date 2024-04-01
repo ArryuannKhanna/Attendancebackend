200668804148a0e93e3f9cd20f89d8dff265b08a
Arryuann Khanna <aryankhannachd@gmail.com>
2024-04-01 21:28:13 +0530

    first commit

--- classroom_management/views.py
+++ classroom_management/views.py
@@ -33,6 +33,7 @@ def JoinClassroom(request,id):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
